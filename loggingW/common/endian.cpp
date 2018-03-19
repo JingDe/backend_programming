843440aa4bbfcb03245592207f01c2e3b75689bb
Norah <wjing931202@163.com>
2018-03-19 19:39:05 +0800

    update

--- loggingW/common/endian.cpp
+++ loggingW/common/endian.cpp
@@ -12,3 +12,35 @@ bool is_big_endian()
 
 	return bint.c[0] == 1;
 }
+
+// 使用char指针指向int的某个字节
+bool is_little_endian()
+{
+	int num = 1;
+	if (*(char *)&num == 1)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+
+#ifdef POSIX
+
+#include <arpa/inet.h>
+bool is_big_endian2()
+{
+	if (htonl(47) == 47) { // 网络字节序是大端
+		// Big endian
+		return true;
+	}
+	else {
+		// Little endian.
+		return false;
+	}
+}
+
+#endif
