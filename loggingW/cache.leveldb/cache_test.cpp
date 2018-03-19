dd433300112d6f316a9fcddcdc3a4d1fb4b5c8b5
Norah <wjing931202@163.com>
2018-03-19 12:33:11 +0800

    update

--- loggingW/cache.leveldb/cache_test.cpp
+++ loggingW/cache.leveldb/cache_test.cpp
@@ -1,6 +1,8 @@
 #include"cache.h"
 #include"common/coding.h"
+#include"common/testharness.h"
 
+#include<iostream>
 #include<cassert>
 #include<vector>
 
@@ -69,12 +71,14 @@ CacheTest* CacheTest::current_;
 
 void test_HitAndMiss()
 {
-	assert(-1 == CacheTest::current_->Lookup(100));
+	CacheTest::current_ = new CacheTest;
+
+	assert(-1 == CacheTest::current_->Lookup(100)); // current_为NULL
 
 	CacheTest::current_->Insert(100, 101);
-	assert(101, Lookup(100));
-	assert(-1, Lookup(200));
-	assert(-1, Lookup(300));
+	assert(101== CacheTest::current_->Lookup(100));
+	assert(-1== CacheTest::current_->Lookup(200));
+	assert(-1== CacheTest::current_->Lookup(300));
 
 	CacheTest::current_->Insert(200, 201);
 	assert(101== CacheTest::current_->Lookup(100));
@@ -89,11 +93,73 @@ void test_HitAndMiss()
 	assert(1== CacheTest::current_->deleted_keys_.size());
 	assert(100== CacheTest::current_->deleted_keys_[0]);
 	assert(101== CacheTest::current_->deleted_values_[0]);
+
+	delete CacheTest::current_;
+
+	printf("passed HitAndMiss\n");
+}
+
+void test_Erase()
+{
+	CacheTest::current_ = new CacheTest;
+
+	CacheTest::current_->Erase(200);
+	assert(0 == CacheTest::current_->deleted_keys_.size());
+
+	CacheTest::current_->Insert(100, 101);
+	CacheTest::current_->Insert(200, 201);
+	CacheTest::current_->Erase(100);
+	assert(-1 == CacheTest::current_->Lookup(100));
+	assert(201== CacheTest::current_->Lookup(200));
+
+	assert(1 == CacheTest::current_->deleted_keys_.size());
+	assert(100 == CacheTest::current_->deleted_keys_[0]);
+	assert(101 == CacheTest::current_->deleted_values_[0]);
+
+	CacheTest::current_->Erase(100);
+	assert(-1 == CacheTest::current_->Lookup(100));
+	assert(201 == CacheTest::current_->Lookup(200));
+	assert(1 == CacheTest::current_->deleted_keys_.size());
+
+	delete CacheTest::current_;
+
+	printf("passed Erase\n");
+}
+
+
+TEST(CacheTest, EntriesArePinned) {
+	Insert(100, 101); // (100, 101)引用计数为2
+	Cache::Handle* h1 = cache_->Lookup(EncodeKey(100)); // (100, 101)引用计数为3
+	//std::cout << reinterpret_cast<LRUHandle*>(h1)->refs << std::endl; 
+	ASSERT_EQ(101, DecodeValue(cache_->Value(h1)));
+
+	Insert(100, 102); // (100, 101)移动到lru_，引用计数为2
+	Cache::Handle* h2 = cache_->Lookup(EncodeKey(100));
+	ASSERT_EQ(102, DecodeValue(cache_->Value(h2)));
+	ASSERT_EQ(0, deleted_keys_.size());
+
+	cache_->Release(h1);
+	ASSERT_EQ(1, deleted_keys_.size());
+	ASSERT_EQ(100, deleted_keys_[0]);
+	ASSERT_EQ(101, deleted_values_[0]);
+
+	Erase(100);
+	ASSERT_EQ(-1, Lookup(100));
+	ASSERT_EQ(1, deleted_keys_.size());
+
+	cache_->Release(h2);
+	ASSERT_EQ(2, deleted_keys_.size());
+	ASSERT_EQ(100, deleted_keys_[1]);
+	ASSERT_EQ(102, deleted_values_[1]);
 }
 
 int main()
 {
 	test_HitAndMiss();
+
+	test_Erase();
+
+	test::RunAllTests();
 
 	system("pause");
 	return 0;
--- loggingW/common/endian.h
+++ loggingW/common/endian.h
@@ -2,9 +2,38 @@
 #define ENDIAN_H_
 
 /*
- π”√gcc
-if
+#undef PLATFORM_IS_LITTLE_ENDIAN
+
+#if defined(__APPLE__)
+	#include <machine/endian.h>
+	#if defined(__DARWIN_LITTLE_ENDIAN) && defined(__DARWIN_BYTE_ORDER)
+	#define PLATFORM_IS_LITTLE_ENDIAN 	(__DARWIN_BYTE_ORDER == __DARWIN_LITTLE_ENDIAN)
+	#endif
+#elif defined(OS_SOLARIS)
+	#include <sys/isa_defs.h>
+	#ifdef _LITTLE_ENDIAN
+	#define PLATFORM_IS_LITTLE_ENDIAN true
+	#else
+	#define PLATFORM_IS_LITTLE_ENDIAN false
+	#endif
+#elif defined(OS_FREEBSD) || defined(OS_OPENBSD) ||  defined(OS_NETBSD) || defined(OS_DRAGONFLYBSD)
+	#include <sys/types.h>
+	#include <sys/endian.h>
+	#define PLATFORM_IS_LITTLE_ENDIAN (_BYTE_ORDER == _LITTLE_ENDIAN)
+#elif defined(OS_HPUX)
+	#define PLATFORM_IS_LITTLE_ENDIAN false
+#elif defined(OS_ANDROID)
+	// Due to a bug in the NDK x86 <sys/endian.h> definition,
+	// _BYTE_ORDER must be used instead of __BYTE_ORDER on Android.
+	// See http://code.google.com/p/android/issues/detail?id=39824
+	#include <endian.h>
+	#define PLATFORM_IS_LITTLE_ENDIAN  (_BYTE_ORDER == _LITTLE_ENDIAN)
+#elif defined(GCC)
+	#include <endian.h>
+	#define PLATFORM_IS_LITTLE_ENDIAN (__BYTE_ORDER == __LITTLE_ENDIAN)
+#endif
 */
+
 
 extern bool is_big_endian();
 
